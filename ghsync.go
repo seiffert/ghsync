2ac156c3528d45a6114c96f0993a134fd750ffcf
Paul Seiffert <paul.seiffert@jimdo.com>
2017-02-05 23:34:43 +0100

    Extract label functions

--- ghsync.go
+++ ghsync.go
@@ -64,7 +64,7 @@ func run(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("Error reading repository list: %s", err)
 	}
 
-	if err := syncLabels(cfg.Labels, repos, c); err != nil {
+	if err := syncLabels(cfg.Labels, repos, c.Issues); err != nil {
 		return fmt.Errorf("Error syncing repositories: %s", err)
 	}
 
@@ -78,86 +78,6 @@ func readInRepositories() ([]string, error) {
 		return nil, err
 	}
 	return strings.Split(strings.Trim(string(repoData), " \n"), "\n"), nil
-}
-
-func syncLabels(labels map[string]string, repos []string, c *github.Client) error {
-	for _, repo := range repos {
-		log.Printf("Processing repository %q", repo)
-		repoParts := strings.Split(repo, "/")
-		if len(repoParts) < 2 {
-			return fmt.Errorf("repository %q is missing the owner. Required format: <owner>/<repository>", repo)
-		}
-
-		repoLabels, err := fetchLabels(repoParts[0], repoParts[1], c)
-		if err != nil {
-			return err
-		}
-
-		log.Printf("  Found %d labels", len(repoLabels))
-
-		for label, color := range labels {
-			if err := ensureLabel(label, color, repoParts[0], repoParts[1], repoLabels, c); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func fetchLabels(owner, repo string, c *github.Client) (map[string]string, error) {
-	repoLabels := map[string]string{}
-	page := 0
-	for {
-		repoLabelsPage, resp, err := c.Issues.ListLabels(owner, repo, &github.ListOptions{
-			Page: page,
-		})
-		if err != nil {
-			return nil, fmt.Errorf("could not list existing labels of repo %q: %s", repo, err)
-		}
-
-		for _, repoLabel := range repoLabelsPage {
-			repoLabels[*repoLabel.Name] = *repoLabel.Color
-		}
-
-		if resp.NextPage == 0 {
-			break
-		}
-		page = resp.NextPage
-	}
-	return repoLabels, nil
-}
-
-func ensureLabel(label, color, owner, repo string, existingLabels map[string]string, c *github.Client) error {
-	if color[0] == '#' {
-		color = color[1:]
-	}
-	if len(color) != 6 {
-		return fmt.Errorf("color %q of label %q for repo \"%s/%s\" is in an invalid format. Colors need to be formatted as six hexadecimal digits", color, label, owner, repo)
-	}
-
-	repoLabelColor, ok := existingLabels[label]
-	if !ok {
-		log.Printf("  Creating label %q: %q", label, color)
-		_, _, err := c.Issues.CreateLabel(owner, repo, &github.Label{
-			Name:  github.String(label),
-			Color: github.String(color),
-		})
-		if err != nil {
-			return fmt.Errorf("could not create label %q in repo \"%s/%s\": %s", label, owner, repo, err)
-		}
-	} else if repoLabelColor != color {
-		log.Printf("  Updating label %q: %q", label, color)
-		_, _, err := c.Issues.EditLabel(owner, repo, label, &github.Label{
-			Name:  github.String(label),
-			Color: github.String(color),
-		})
-		if err != nil {
-			return fmt.Errorf("could not update label %q in repo \"%s/%s\": %s", label, owner, repo, err)
-		}
-	} else {
-		log.Printf("  Label %q: %q already exists", label, color)
-	}
-	return nil
 }
 
 func must(err error) {
